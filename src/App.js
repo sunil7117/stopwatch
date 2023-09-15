e50e80953dd004d0d4931a00f55e7c5ff6d82051
sunil7117 <sunilapp7117@gmail.com>
2023-09-15 13:38:01 +0530

    inital comment for stopwatch that returns hour minute second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import './App.css'
 function App() {
 return (
     <div className="App">
       <h1>Lets create a stopwatch</h1>
+      <h3>Timer that show hours,Min and seconds....</h3>
     </div>
 
   );
