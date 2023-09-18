53f2e7c78c8a5c52ade12fadf33309f8f7077840
sunil7117 <sunilapp7117@gmail.com>
2023-09-18 14:14:33 +0530

    change stopwatch settings and margin in button

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import { useEffect, useState } from 'react';
 import './App.css'
 function App() {
   const[timer,setTimer]=useState(0)
+  const[hh,sethh]=useState(0)
+  const[mm,setmm]=useState(0)
+  const[ss,setss]=useState(0)
   const[start,setStart]=useState(false)
+  const[starthh,setStarthh]=useState(false)
   const[stop,setStop]=useState(true)
+  const[stophh,setStophh]=useState(true)
   const[reset,setReset]=useState(true)
+  const[resethh,setResethh]=useState(true)
   const timerClick=(btn)=>{
     switch(btn){
       case 'startSec':{
@@ -13,10 +19,22 @@ function App() {
         setReset(false)
         break
       }
+      case 'starthh':{
+        setStarthh(true)
+        setStophh(false)
+        setResethh(false)
+        break
+      }
       case 'stopSec':{
         setStart(false)
         setStop(true)
         setReset(false)
+        break
+      }
+      case 'stophh':{
+        setStarthh(false)
+        setStophh(true)
+        setResethh(false)
         break
       }
       case 'resetSec':{
@@ -26,6 +44,18 @@ function App() {
         setReset(true)
         break
       }
+      case 'resethh':{
+        sethh(0)
+        setmm(0)
+        setss(0)
+        // setStart(false)
+        // setStop(true)
+        // setReset(true)
+        setStarthh(false)
+        setStophh(true)
+        setResethh(true)
+        break
+      }
       default:{
       }
     }
@@ -33,22 +63,42 @@ function App() {
   }
   
   useEffect(()=>{
+    if(ss===59){
+      setss(0)
+      setmm(mm+1)
+    }
+    if(mm===59){
+      setmm(0)
+      sethh(hh+1)
+    }
     if(start){
       const secInterval=setInterval(()=>setTimer((prev)=>prev+1),1000)
     return()=>clearInterval(secInterval)
     }
-    
-  },[start])
+    if(starthh){
+      const secInterval=setInterval(()=>setss((prev)=>prev+1),1000)
+    return()=>clearInterval(secInterval)
+  }
+  },[start ,starthh,ss,mm,hh])
   
   return (
     <div className="App">
       <h1>Lets create a stopwatch</h1>
       <h3>Timer based on second.</h3>
       <h1>{timer}</h1>
-      <button style={{marginRight:'10px'}} disabled={start} onClick={()=>timerClick('startSec')}>Start</button>
-      <button style={{marginRight:'10px'}} disabled={stop} onClick={()=>timerClick('stopSec')}>Stop</button>
+      <button disabled={start} onClick={()=>timerClick('startSec')}>Start</button>
+      <button disabled={stop} onClick={()=>timerClick('stopSec')}>Stop</button>
       <button disabled={reset} onClick={()=>timerClick('resetSec')}>Reset</button>
+
+
+
+
+
       <h3>Timer that show hours,Min and seconds....</h3>
+      <h1>{hh}:{mm}:{ss}</h1>
+      <button disabled={starthh} onClick={()=>timerClick('starthh')}>Start</button>
+      <button disabled={stophh} onClick={()=>timerClick('stophh')}>Stop</button>
+      <button disabled={resethh} onClick={()=>timerClick('resethh')}>Reset</button>
     </div>
 
   );
